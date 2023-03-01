c44197b6f94cc5c78018618d7ac91aa965307fe3
Debapriyo Das <115923073+debapriyo007@users.noreply.github.com>
2023-03-01 17:13:52 +0530

    01-03-2023 17:13

---
+++ St_Challenge/arr_obj.java
@@ -0,0 +1,73 @@
+
+
+//import java.util.*;
+//package St_Challenge;
+
+//import javax.security.auth.Subject;
+
+class subject{
+    public String subId;
+    public String name;
+    public int maxMarks;
+    public int maxObtain;
+
+
+    //properties.....
+    public String  getSubID(){
+        return subId;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getmaxMarks(){
+        return maxMarks;
+    }
+
+    public int getmaxObtain(){
+        return maxObtain;
+    }
+
+    //setx part
+
+    public void  setmaxMarks(int m){
+
+       maxMarks=m;
+    }
+    public void  setmaxObtain(int n){
+        maxObtain=n;
+    }
+
+    public boolean isQulify(){
+        return maxObtain>=maxMarks/10*4;
+    }
+
+    public String toString(){
+        return "\nSubjectId:"+subId+"\nName:"+name+"\nMarksObtain:"+maxObtain;
+    }
+    
+
+
+
+    //constructor...
+
+    public subject(String subId , String name, int maxObtain){
+        this.subId= subId;
+        this.name= name;
+        this.maxObtain=maxObtain;
+    }
+}
+
+public class arr_obj {
+    public static void main(String[] args) {
+       subject sub[]= new subject[3];
+       sub[0]= new subject("pcc-192","Opearating System", 97);
+       sub[1]= new subject("cs-85","Algorithim ",99);
+       sub[2]= new subject("bsc-404","Analog",70);
+       for(subject s:sub){
+         System.out.println(s);
+       }
+        
+    }
+    
+}
