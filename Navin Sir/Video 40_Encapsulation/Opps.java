d8fd1cd2b4fced0c19b375fa4b56f432e0aaf0af
Debapriyo Das <debapriyo00@gmail.com>
2024-02-14 15:31:29 +0530

    Update Enacpsulation

---
+++ Navin Sir/Video 40_Encapsulation/Opps.java
@@ -0,0 +1,45 @@
+class Humans{
+    private int age; //here default age is = 0
+    //"Private"  varriable is only accessable inside this class only "Humans" class only
+    private String name; //default name is = null
+
+    //we aaccess this "private" variable through some Methords.
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
+
+    public void setname(String n){
+        name = n;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
+
+public class Opps {
+    public static void main(String[] args) {
+        Humans obj = new Humans();
+
+        // obj.age = 21;
+        // obj.name = "Debu";
+
+        obj.setAge(21);
+        obj.setname("Debu");
+        System.out.println("Name : " + obj.getName() + " Age : " + obj.getAge()); 
+        //but ,  What if  i can't share my age
+    }
+}
+
+
